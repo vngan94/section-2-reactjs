d52c08449eec855497e77fca9253185b91fe85b7
Vo Ngan <vongan240005@gmail.com>
2026-02-15 21:23:48 +0700

    refactor todo Service

---
+++ src/services/todoService.js
@@ -0,0 +1,38 @@
+let todos = [
+  { id: 1, title: "Complete project documentation", completed: false },
+  { id: 2, title: "Review pull requests", completed: true },
+  { id: 3, title: "Update dependencies", completed: false },
+];
+
+let nextId = 4;
+
+export const getTodos = () => {
+  return [...todos];
+};
+
+export const addTodo = (title) => {
+  const newTodo = {
+    id: nextId++,
+    title,
+    completed: false,
+  };
+  todos.push(newTodo);
+  return newTodo;
+};
+
+export const toggleTodo = (id) => {
+  const todo = todos.find((t) => t.id === id);
+  if (todo) {
+    todo.completed = !todo.completed;
+  }
+  return todo;
+};
+
+export const deleteTodo = (id) => {
+  todos = todos.filter((t) => t.id !== id);
+};
+
+export const clearTodos = () => {
+  todos = [];
+  nextId = 1;
+};
